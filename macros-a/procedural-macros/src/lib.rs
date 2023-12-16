5e43baf1012d99f8d4dba6fa0986900a122d5e10
Egal <wangxiangyi@ipfsa.io>
2023-12-16 21:34:05 +0800

    过程宏: CustomDebug

--- macros-a/procedural-macros/src/lib.rs
+++ macros-a/procedural-macros/src/lib.rs
@@ -1,4 +1,4 @@
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream, TokenTree};
 use quote::quote;
 use syn::{DeriveInput, parse_macro_input};
 
@@ -6,7 +6,6 @@ use syn::{DeriveInput, parse_macro_input};
 pub fn hello_macro_dervie(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let name = &ast.ident;
-
     let gen = quote! {
         impl HelloMacro for #name {
             fn hello_macro() {
@@ -15,4 +14,52 @@ pub fn hello_macro_dervie(input: TokenStream) -> TokenStream {
         }
     };
     gen.into()
+}
+
+#[proc_macro_derive(CustomDebug)]
+pub fn custom_debug(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
+    let gen = quote! {
+        impl std::fmt::Debug for #name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                // Customize the debug output for your type here
+                f.debug_struct(stringify!(#name))
+                    .field("field1", &self.field1)
+                    .field("field2", &self.field2)
+                    .finish()
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_attribute]
+pub fn test_macro(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // `args` 用于接收属性参数的输入流，
+    println!("attr: {:?}", attr);
+    // `item` 用于接收被处理的输入流。
+    println!("item: {:?}", item);
+
+    let attr_vec: Vec<TokenTree> = attr.clone().into_iter().collect();
+    println!("attr_vec: {:?}", attr_vec);
+    let item_vec: Vec<TokenTree> = item.clone().into_iter().collect();
+    println!("item_vec: {:?}", item_vec);
+    item
+}
+
+#[proc_macro_attribute]
+pub fn my_main(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // 解析属性宏
+    let mut args = attr.into_iter();
+    let value = args.next().unwrap();
+    println!("value = {:#?}", value.span().source_text());
+
+    // 替换掉我们自己的代码
+    // 输出一条语句，输出的内容是属性定义的值
+    let modified_item = format!("fn main() {{ print!(\"Hello, \"); println!({}); }}",value);
+    println!("modified_item: {:#?}", modified_item);
+
+    // 返回我们自己的代码
+    modified_item.parse().unwrap()
 }
--- macros-a/src/main.rs
+++ macros-a/src/main.rs
@@ -1,8 +1,38 @@
-use procedural_macros::HelloMacro;
+use procedural_macros::{CustomDebug, HelloMacro, my_main, test_macro};
 
 // #[derive(HelloMacro)]
 // struct Pancakes;
-//
+
+
+
+// #[my_main("Rust_main")]
+// fn main() {
+//     Pancakes::hello_macro();
+//     // test_macro_test();
+// }
+
+/// 执行cargo build后，在控制台输出如下
+///
+/// 参数 attr 就是属性宏 #[test_macro(name = "zhangsan", age = 18)] 的分解：
+/// attr: TokenStream [Ident { ident: "name", span: #0 bytes(158..162) }, Punct { ch: '=', spacing: Alone, span: #0 bytes(163..164) }, Literal { kind: Str, symbol: "MikiOng", suffix: None, span: #0 bytes(165..174) }, Punct { ch: ',', spacing: Alone, span: #0 bytes(174..175) }, Ident { ident: "age", span: #0 bytes(176..179) }, Punct { ch: '=', spacing: Alone, span: #0 bytes(180..181) }, Literal { kind: Integer, symbol: "18", suffix: None, span: #0 bytes(182..184) }]
+///
+/// 参数item 对代码的分解：
+/// item: TokenStream [Ident { ident: "fn", span: #0 bytes(187..189) }, Ident { ident: "test_macro_test", span: #0 bytes(190..205) }, Group { delimiter: Parenthesis, stream: TokenStream [], span: #0 bytes(205..207) }, Group { delimiter: Brace, stream: TokenStream [Ident { ident: "println", span: #0 bytes(214..221) }, Punct { ch: '!', spacing: Alone, span: #0 bytes(221..222) }, Group { delimiter: Parenthesis, stream: TokenStream [Literal { kind: Str, symbol: "Hello, World!", suffix: None, span: #0 bytes(223..238) }], span: #0 bytes(222..239) }, Punct { ch: ';', spacing: Alone, span: #0 bytes(239..240) }], span: #0 bytes(208..242) }]
+#[test_macro(name = "MikiOng", age = 18)]
+fn test_macro_test() {
+    println!("Hello, World!");
+}
+
+#[derive(CustomDebug)]
+struct MyStruct {
+    field1: i32,
+    field2: String,
+}
+
 fn main() {
-    // Pancakes::hello_macro();
+    let my_struct = MyStruct {
+        field1: 42,
+        field2: "Hello, world!".into(),
+    };
+    println!("{:#?}", my_struct);
 }
