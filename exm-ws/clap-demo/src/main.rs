b550beca59a0aefcb69e8fd5ce0edce1897434eb
Egal <wangxiangyi@ipfsa.io>
2024-04-17 19:51:50 +0800

    clap cli: arg short long

--- exm-ws/clap-demo/src/main.rs
+++ exm-ws/clap-demo/src/main.rs
@@ -1,19 +1,24 @@
 use clap::Parser;
 
 #[derive(Parser)]
+#[command(name = "zimu")]
+#[command(author = "zimu.top")]
+#[command(version = "1.0")]
+#[command(about = "a tutorial of crate clap", long_about = None)]
 struct Cli {
     // 注意下面的注释是三个斜杠!!!
-    /// argument of name
-    name: String,
-    /// argument of names
-    names: Vec<String>,
+    /// use which method
+    #[arg(short, long)]
+    method: Option<String>,
+
+    /// Optional name to call
+    name: Option<String>,
 }
 
-/// cargo run -- zim name1 name2
+/// cargo run -- -m zim name1
 fn main() {
     let cli = Cli::parse();
-    let name = cli.name;
-    let names = cli.names;
-    println!("name: {name}");
-    println!("names: {names:?}");
+    let method = cli.method.unwrap_or_else(|| "hello".to_owned());
+    let name = cli.name.unwrap_or_else(|| "world".to_owned());
+    println!("{method} {name}");
 }
