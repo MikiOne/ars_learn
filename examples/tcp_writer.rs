650b01458e67d830bf4210e518c2dd823cf6be80
Egal <wangxiangyi@ipfsa.io>
2024-04-29 20:52:24 +0800

    trait demo

---
+++ examples/tcp_writer.rs
@@ -0,0 +1,23 @@
+use std::io::{BufWriter, Write};
+use std::net::TcpStream;
+
+#[derive(Debug)]
+struct MyWriter<W> {
+    writer: W,
+}
+
+impl<W: Write> MyWriter<W> {
+    pub fn new(addr: &str) -> Self {
+        let stream = TcpStream::connect("127.0.0.1:8080").unwrap();
+        Self { writer: BufWriter::<W>::new(stream) }
+    }
+
+    pub fn write(&mut self, buf: &str) -> std::io::Result<()> {
+        self.writer.write_all(buf.as_bytes())
+    }
+}
+
+fn main() {
+    let mut writer = MyWriter::new("127.0.0.1:8080");
+    writer.write("hello world!");
+}
