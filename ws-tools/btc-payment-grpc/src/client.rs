935c9212a5652a09c84c49238341ab9c3f366dd1
egal <mikione@outlook.com>
2024-05-13 14:26:32 +0800

    Bitcoin payment grpc demo

---
+++ ws-tools/btc-payment-grpc/src/client.rs
@@ -0,0 +1,18 @@
+use crate::bitcoin_client::BitcoinClient;
+tonic::include_proto!("payments");
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // http://[::1]:50051
+    let mut client = BitcoinClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(BtcPaymentRequest {
+        from_addr: "123".to_string(),
+        to_addr: "321".to_string(),
+        amount: 10,
+    });
+
+    let resp = client.send_payment(request).await?;
+    println!("Response: {:?}", resp);
+    Ok(())
+}
