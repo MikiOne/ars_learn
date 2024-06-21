cc5ab7003b2a4c8eb22257f4117a46cc1cade7ae
egal <egal@qq.com>
2024-06-21 13:06:13 +0800

    Rust中实现事件循环模式，跟着学 test

--- ws-rs/crossbeam-a/src/main.rs
+++ ws-rs/crossbeam-a/src/main.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
 use crossbeam::channel;
 use crossbeam::channel::{Receiver, Sender};
 use strum_macros::{Display, EnumString};
@@ -8,12 +10,15 @@ use strum_macros::{Display, EnumString};
 enum Event {
     Default,
     EventTest,
+    DBEvent,
 }
 
 type Payload = Vec<u8>;
 
 trait EventHandler: Send + Sync {
-    fn handle(&self, event: &Event, payload: &Payload);
+    fn handle(&self, event: &Event, payload: &Payload) {
+        println!("Default event {} => {}", event, String::from_utf8(payload.clone()).unwrap());
+    }
 }
 
 struct Dispatcher {
@@ -49,9 +54,10 @@ impl Dispatcher {
             loop {
                 if let Ok((event, payload)) = rx.recv() {
                     let reg_map = registry.lock().unwrap();
-                    let handlers = reg_map.get(&event).unwrap();
-                    for handler in handlers {
-                        handler.handle(&event, &payload);
+                    if let Some(handlers) = reg_map.get(&event) {
+                        for handler in handlers {
+                            handler.handle(&event, &payload);
+                        }
                     }
                 }
             }
@@ -59,6 +65,34 @@ impl Dispatcher {
     }
 }
 
+struct TestEventHandler;
+impl EventHandler for TestEventHandler {
+    fn handle(&self, event: &Event, payload: &Payload) {
+        println!("Test event: {:?}, payload: {:?}", event, payload);
+    }
+}
+
+struct DBEventHandler;
+impl EventHandler for DBEventHandler {
+    fn handle(&self, event: &Event, payload: &Payload) {
+        println!("DB event: {:?}, payload: {:?}", event, payload);
+    }
+}
+
 
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    let dispatcher = Dispatcher::new();
+    dispatcher.register_handler(Event::EventTest, Arc::new(TestEventHandler));
+    dispatcher.register_handler(Event::DBEvent, Arc::new(DBEventHandler));
+    dispatcher.start();
+
+    dispatcher.trigger_event(Event::Default, vec![1, 2, 3]);
+    dispatcher.trigger_event(Event::EventTest, vec![4, 5, 6]);
+    dispatcher.trigger_event(Event::DBEvent, vec![7, 8, 9]);
+    dispatcher.trigger_event(Event::EventTest, vec![10, 11, 12]);
+    dispatcher.trigger_event(Event::DBEvent, vec![13, 14, 15]);
+    dispatcher.trigger_event(Event::EventTest, vec![16, 17, 18]);
+
+    tokio::time::sleep(Duration::from_millis(20)).await
+}
