77fc34b89c1b34dfdde7bb7d900f35987e6fe79b
egal <egal@qq.com>
2024-08-14 11:16:17 +0800

    SelfReference pin

--- ws-tyr/future-a/examples/pin_self_ref_exam.rs
+++ ws-tyr/future-a/examples/pin_self_ref_exam.rs
@@ -1,36 +1,40 @@
-use std::marker::PhantomPinned;
-use std::pin::Pin;
+use std::{marker::PhantomPinned, pin::Pin};
 
 #[derive(Debug)]
 struct SelfReference {
     name: String,
+    // 在初始化后指向 name
     name_ptr: *const String,
+    // PhantomPinned 占位符
     _marker: PhantomPinned,
 }
 
 impl SelfReference {
-    fn new(name: impl Into<String>) -> Self {
-        Self {
+    pub fn new(name: impl Into<String>) -> Self {
+        SelfReference {
             name: name.into(),
             name_ptr: std::ptr::null(),
             _marker: PhantomPinned,
         }
     }
 
-    fn init(self: Pin<&mut Self>) {
+    pub fn init(self: Pin<&mut Self>) {
         let name_ptr = &self.name as *const String;
+        // SAFETY: 这里并不会把任何数据从 &mut SelfReference 中移走
         let this = unsafe { self.get_unchecked_mut() };
         this.name_ptr = name_ptr;
     }
 
-    fn print_name(self: Pin<&Self>) {
+    pub fn print_name(self: Pin<&Self>) {
         println!(
-            "struct_ptr {:p}: Ptr(name: {:p} name_ptr: {:p}), Val(name: {}, name_ref: {})",
+            "struct {:p}: (name: {:p} name_ptr: {:p}), name: {}, name_ref: {}",
             self,
             &self.name,
             self.name_ptr,
             self.name,
-            unsafe { &*self.name_ptr }
+            // 在使用 ptr 是需要 unsafe
+            // SAFETY: 因为数据不会移动，所以这里 name_ptr 是安全的
+            unsafe { &*self.name_ptr },
         );
     }
 }
@@ -40,25 +44,23 @@ fn main() {
 }
 
 fn move_creates_issue() {
-    let mut data = SelfReference::new("Hello");
+    let mut data = SelfReference::new("Tyr");
     let mut data = unsafe { Pin::new_unchecked(&mut data) };
-    // SelfReference::init(data.as_mut());
-    data.as_mut().init();
+    SelfReference::init(data.as_mut());
 
     // 不 move，一切正常
     data.as_ref().print_name();
 
     // 现在只能拿到 pinned 后的数据，所以 move 不了之前
     move_pinned(data.as_mut());
-    println!("move_create: {:?} ptr({:p})", data, &data);
+    println!("{:?} ({:p})", data, &data);
 
     // 你无法拿回 Pin 之前的 SelfReference 结构，所以调用不了 move_it
-    // move_it(data);
+    move_it(data);
 }
 
-
 fn move_pinned(data: Pin<&mut SelfReference>) {
-    println!("move_pinned: {:?} ptr({:p})", data, &data);
+    println!("{:?} ({:p})", data, &data);
 }
 
 #[allow(dead_code)]
