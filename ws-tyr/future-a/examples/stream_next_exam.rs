4b3a6d2c0a450c02ee8826b71707c506ca6d1cfb
egal <egal@qq.com>
2024-08-14 16:15:10 +0800

    40 异步处理：如何处理异步IO: stream map sleep

--- ws-tyr/future-a/examples/stream_next_exam.rs
+++ ws-tyr/future-a/examples/stream_next_exam.rs
@@ -1,13 +1,14 @@
-use std::{thread, time};
-use futures::prelude::*;
+use std::thread;
 use std::time::Duration;
+
+use futures::prelude::*;
 
 #[tokio::main]
 async fn main() {
     let mut st = stream::iter(1..10)
         .filter(|x| future::ready(x % 2 == 0))
         .map(|x| {
-            thread::sleep(Duration::from_secs(1));
+            thread::sleep(Duration::from_millis(300));
             x * x
         });
 
