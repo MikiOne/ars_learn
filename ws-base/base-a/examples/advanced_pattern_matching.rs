a12c890b750b6e52de09766ef0aa68de6d22d579
egal <egal@qq.com>
2024-08-20 16:00:14 +0800

    Advanced pattern matching

---
+++ ws-base/base-a/examples/advanced_pattern_matching.rs
@@ -0,0 +1,24 @@
+enum OptionalI32 {
+    AnI32(i32),
+    Nothing,
+}
+
+struct FooBar {
+    x: i32,
+    y: OptionalI32,
+}
+
+fn main() {
+    let bar = FooBar { x: 15, y: OptionalI32::AnI32(32) };
+
+    match bar {
+        FooBar { x: 0, y: OptionalI32::AnI32(0) } =>
+            println!("The numbers are zero!"),
+        FooBar { x: n, y: OptionalI32::AnI32(m) } if n == m =>
+            println!("The numbers are the same"),
+        FooBar { x: n, y: OptionalI32::AnI32(m) } =>
+            println!("Different numbers: {} {}", n, m),
+        FooBar { x: _, y: OptionalI32::Nothing } =>
+            println!("The second number is Nothing!"),
+    }
+}
